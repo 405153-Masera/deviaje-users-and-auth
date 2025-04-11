f968dda65ea6ffb986de987f28a9dc30b3294f58
405153-Masera <405153@tecnicatura.frc.utn.edu.ar>
2025-04-11 07:20:18 -0300

    first commit

---
+++ src/main/java/masera/deviajeusersandauth/DeviajeUsersAndAuthApplication.java
@@ -0,0 +1,13 @@
+package masera.deviajeusersandauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviajeUsersAndAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviajeUsersAndAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/masera/deviajeusersandauth/DeviajeUsersAndAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package masera.deviajeusersandauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviajeUsersAndAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
